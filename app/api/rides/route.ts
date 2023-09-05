68094acec688bec16aac337ed3a258b6768198a5
mrchl55 <37666744+mrchl55@users.noreply.github.com>
2023-09-05 16:16:08 +0200

    code cleanup

--- app/api/rides/route.ts
+++ app/api/rides/route.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-// Fetch all posts (in /pages/api/posts.ts)
 const prisma = new PrismaClient();
 
 export async function GET() {
